bb9c71adb2646b52cbe4a52c33583a31464a6128
Hunter King <hking6776@gmail.com>
2023-02-22 12:53:11 -0700

    edit env vars

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,24 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore/lite';
-// import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDGyokcjIYqQ8CTLJCzosP70uTCJ8yooRg',
-  authDomain: 'portfolio-1f6fc.firebaseapp.com',
-  projectId: 'portfolio-1f6fc',
-  storageBucket: 'portfolio-1f6fc.appspot.com',
-  messagingSenderId: '356584625176',
-  appId: '1:356584625176:web:eeee399168e6d6bd919f6d',
-  measurementId: 'G-YEFBTFH50V',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_DOMAIN,
+  projectId: process.env.FIREBASE_ID,
+  storageBucket: process.env.FIREBASE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASURE_ID,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore(app);
-
-// const analytics = getAnalytics(app);
