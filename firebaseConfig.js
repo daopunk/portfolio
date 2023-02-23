4d430e14610e27dca2ac4d426b245587fb02042d
Hunter King <hking6776@gmail.com>
2023-02-23 15:49:04 -0700

    env vars

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,14 +1,24 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore/lite';
 
+// const firebaseConfig = {
+//   apiKey: process.env.APIKEY,
+//   authDomain: process.env.AUTHDOMAIN,
+//   projectId: process.env.PROJECTID,
+//   storageBucket: process.env.STORAGEBUCKET,
+//   messagingSenderId: process.env.MESSAGINGSENDERID,
+//   appId: process.env.APPID,
+//   measurementId: process.env.MEASUREMENTID,
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID,
-  measurementId: process.env.MEASUREMENTID,
+  apiKey: 'AIzaSyBQ-NbnzszRiVsvnoRvpvibXac0tgJdBxo',
+  authDomain: 'portfolio-f7e60.firebaseapp.com',
+  projectId: 'portfolio-f7e60',
+  storageBucket: 'portfolio-f7e60.appspot.com',
+  messagingSenderId: '129662105169',
+  appId: '1:129662105169:web:34915e09acf3078f336a70',
+  measurementId: 'G-9V4VFFYL1K',
 };
 
 export const app = initializeApp(firebaseConfig);
